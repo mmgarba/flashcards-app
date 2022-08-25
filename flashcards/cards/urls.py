15c130d840fc7a55bd311285d36f7cb2cacafdca
Mustapha M Garba <mustaphamgarba@outlook.com>
2022-08-25 12:35:03 +0100

    created initial cards

--- flashcards/cards/urls.py
+++ flashcards/cards/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
+
 
 urlpatterns = [
+
     path(
+
         "",
-        TemplateView.as_view(template_name="cards/base.html"),
-        name="home"
+
+        views.CardListView.as_view(),
+
+        name="card-list"
+
     ),
+
 ]
--- flashcards/cards/views.py
+++ flashcards/cards/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import(
+    ListView,
+)
+from .models import Card
+
+class CardListView(ListView):
+    model = Card
+    queryset = Card.objects.all().order_by("box", "-date_created")
